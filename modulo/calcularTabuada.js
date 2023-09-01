f39e25d437663b209cccb3ab6c9eb91038d385b4
Maria Luiza <maria.luiza.costa.balieiro@gmail.com>
2023-09-01 17:17:12 -0300

    em andamento

---
+++ modulo/calcularTabuada.js
@@ -0,0 +1,39 @@
+// ********************************************************************************
+// Objetivo: realizar o calculo de uma tabuada através de estruturas de repetição *
+// data: 01/09/2023                                                               *
+// autor: Maria Luiza                                                             *
+// vrsão: 1.0                                                                     *
+// ********************************************************************************
+
+
+// função para gerar uma tabuada 
+const gerarTabuada = function(valorTabuada, contadorInicial, contadorFinal){
+// etrada de dados que vieram pelos argumentos das funções 
+    let tabuada = valorTabuada;
+    let contInicial = contadorInicial;
+    let contFinal = contadorFinal;
+    
+    let resultado;
+
+   
+    if(contFinal< contInicial){
+        // validação os valores em ordem crescente
+        contInicial=contFinal;
+        contFinal=contInicial
+    
+    } if (contInicial == ''|| contFinal == ''|| tabuada == ''){
+        console.log('ERRO: está vazio :(')
+    
+    }else if (isNaN(contInicial) || isNaN(contFinal)|| isNaN(tabuada)){
+        console.log('ERRO: só é valida a entrada de números')
+    } 
+    
+    else{
+        contInicial = Number(contInicial);
+        contInicial = Number(contFinal);
+        
+        while(contInicial <= contFinal){
+            resultado = tabuada * contInicial;
+        }
+    }
+}
